34e010a23c5f679caa49cc1035e9ff3f45611fc1
Eli Gundry <eligundry@gmail.com>
2011-10-10 14:36:12 -0400

    Added inital project layout for Lab6_1

---
+++ Lab6_1/file_fixer.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author      : Eli Gundry
+ * Date        : 10/10/2011
+ * Name        : File Fixer
+ * Description : Fixes the output of a file
+ * Link        : http://www.cs.kent.edu/~lucc/ioop/Labs/Lab2/proj-8.dat 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	return 0;
+}
