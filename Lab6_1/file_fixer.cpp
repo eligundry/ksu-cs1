835c3db09dd4d0a633bd7eb8abf469f2e26dcd82
Eli Gundry <eligundry@gmail.com>
2011-10-12 17:36:32 -0400

    Modified my Lab6_1

--- Lab6_1/file_fixer.cpp
+++ Lab6_1/file_fixer.cpp
@@ -9,9 +9,14 @@
 #include <iostream>
 #include <fstream>
 
-using namespace std;
-
 int main()
 {
+	using namespace std;
+	ifstream in_stream;
+	ofstream out_stream;
+
+	in_stream.open("proj-8.dat");
+
+	in_stream.close();
 	return 0;
 }
