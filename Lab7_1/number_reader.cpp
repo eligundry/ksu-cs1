a99d1af9e92daa147dcf6f239f26de88a8e415cd
Eli Gundry <eligundry@gmail.com>
2011-10-24 14:21:19 -0400

    Added inital structure of Lab7_1

---
+++ Lab7_1/number_reader.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author      : Eli Gundry
+ * Date        : 10/24/2011
+ * Name        : Number Reader
+ * Description : Finds the numbers in a file or user's input
+ * Link        : http://www.cs.kent.edu/~lucc/ioop/Labs/Lab2/proj-7.dat 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	return 0;
+}
