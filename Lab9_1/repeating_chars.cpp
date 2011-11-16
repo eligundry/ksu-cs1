fc27a490c516ec271d2398a84eaf3dcbc1e243ca
Eli Gundry <eligundry@gmail.com>
2011-11-16 18:10:19 -0500

    Completed Lab9_1

--- Lab9_1/repeating_chars.cpp
+++ Lab9_1/repeating_chars.cpp
@@ -7,36 +7,41 @@
  */
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-void remove_char(char* input);
+typedef char* char_pointer;
+char_pointer remove_char(string user_input);
 // Takes an array of characters and returns array with only first occurrence
 
 int main()
 {
-	const int INPUT_LENGTH = 200;
-	char user_input [INPUT_LENGTH];
-
-	cout << "Please enter the string of characters you would like evaluated" << endl;
-	cin.getline(user_input, (INPUT_LENGTH - 1));
-	
-	remove_char(user_input);
-
+	string user_input;
+	cout << "Please enter your sentence:" << endl;
+	getline(cin, user_input);
+	cout << remove_char(user_input) << endl;
 	return 0;
 }
 
-void remove_char(char* input)
+char_pointer remove_char(string user_input)
 {
-	int array_length = 0,
-		a = 0,
-		i = 0;
+	// Optimize the size of the character array for the worst case scenario
+	char_pointer char_p = new char[user_input.size()];
+	bool is_repeat = false;
 
-	// Figure out the max length of the new char array
-	while (*input++) {
-		++array_length;
+	for (int i = 0, j = 0, p_size = 0; i < user_input.size(); i++, is_repeat = false, j = 0) {
+		do {
+			if (user_input.at(i) == char_p[j]) {
+				is_repeat = true;
+			}
+			++j; 
+		} while (j < p_size && !is_repeat);
+
+		if (!is_repeat) {
+			char_p[p_size] = user_input.at(i);
+			++p_size;
+		}
 	}
-
-	// Initialize array with worst case length
-	char returned_output [array_length];
+	return char_p;
 }
