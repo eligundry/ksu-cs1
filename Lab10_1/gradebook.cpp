9569dbc8d41e597a94cce6a8d2a173b432ec00d5
Eli Gundry <eligundry@gmail.com>
2011-11-21 15:49:22 -0500

    Inital structure of lab 10_1

---
+++ Lab10_1/gradebook.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author      : Eli Gundry
+ * Date        : 11/21/2011
+ * Name        : Gradebook
+ * Description : Determines the letter grade for a student's test
+ * Link        : http://www.cs.kent.edu/~lucc/ioop/Labs/Lab2/proj-13 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	return 0;
+}
