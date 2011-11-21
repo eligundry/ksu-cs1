6697e9935d66ca1079bfda7f9ad31d03c02441fa
Eli Gundry <eligundry@gmail.com>
2011-11-21 15:56:44 -0500

    Added the grade prompts

--- Lab10_1/gradebook.cpp
+++ Lab10_1/gradebook.cpp
@@ -7,10 +7,25 @@
  */
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+	string student_name;
+	double quiz_1, quiz_2, midterm, final;
+
+	cout << "Please enter the student's name: ";
+	cin >> student_name;
+	cout << "Enter " << student_name << "'s first quiz score: ";
+	cin >> quiz_1;
+	cout << "And there second quiz score? ";
+	cin >> quiz_2;
+	cout << "How did they do on the midterm? ";
+	cin >> midterm;
+	cout << "What was there final score? ";
+	cin >> final;
+
 	return 0;
 }
