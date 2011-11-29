040d9176dde367d9655db775de0442f423563202
Eli Gundry <eligundry@gmail.com>
2011-11-28 20:20:56 -0500

    Completed lab10_1

--- Lab10_1/gradebook.cpp
+++ Lab10_1/gradebook.cpp
@@ -7,25 +7,59 @@
  */
 
 #include <iostream>
-#include <string>
 
 using namespace std;
 
+struct student_grade {
+	double quiz_1,
+		quiz_2,
+		midterm,
+		final;
+};
+
+double final_grade(student_grade value);
+char letter_grade(double grade);
+
 int main()
 {
-	string student_name;
-	double quiz_1, quiz_2, midterm, final;
+	student_grade grade;
 
-	cout << "Please enter the student's name: ";
-	cin >> student_name;
-	cout << "Enter " << student_name << "'s first quiz score: ";
-	cin >> quiz_1;
-	cout << "And there second quiz score? ";
-	cin >> quiz_2;
-	cout << "How did they do on the midterm? ";
-	cin >> midterm;
-	cout << "What was there final score? ";
-	cin >> final;
+	cout << "What was the student's grade for quiz 1? (0-10)" << endl;
+	cin >> grade.quiz_1;
+	cout << "And for quiz 2? (0-10)" << endl;
+	cin >> grade.quiz_2;
+	cout << "How did they do on the midterm? (0-100)" << endl;
+	cin >> grade.midterm;
+	cout << "And the final exam? (0-100)" << endl;
+	cin >> grade.final;
+
+	cout << endl << "Quiz 1: " << (grade.quiz_1 * 10) << endl
+		 << "Quiz 2: " << (grade.quiz_2 * 10) << endl
+		 << "Midterm: " << (grade.midterm) << endl
+		 << "Final Exam: " << (grade.final) << endl << endl;
+
+	double final_score = final_grade(grade);
+
+	cout << "Student's final grade is " << final_score << " which is a(n) " << letter_grade(final_score) << endl;
 
 	return 0;
+}
+
+double final_grade(student_grade value)
+{
+	return ((value.final * 0.5) + (value.midterm * 0.25) + (((value.quiz_1 * 5) + (value.quiz_1 * 5)) * 0.25));
+}
+
+char letter_grade(double grade)
+{
+	if (grade >= 90)
+		return 'A';
+	else if (grade >= 80)
+		return 'B';
+	else if (grade >= 70)
+		return 'C';
+	else if (grade >= 60)
+		return 'D';
+	else
+		return 'F';
 }
