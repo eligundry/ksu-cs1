cac5aacdb4cdbfea31adcdde58f1b54cae0442bb
Eli Gundry <eligundry@gmail.com>
2011-12-13 14:40:17 -0500

    Updated my lab 11

--- Lab11/fractional_computation.h
+++ Lab11/fractional_computation.h
@@ -1,9 +1,14 @@
 using namespace std; 
 
+/*
+ * do {
+ *     fml;
+ * } while (alive);
+ */
+
 // Created my own fraction data type so everything can be returned easily
 struct fraction {
-	int numerator,
-		denominator;
+	int numerator, denominator;
 };	 
 
 class FractionalComputation
@@ -15,45 +20,49 @@ class FractionalComputation
 	public:
 		void input(FractionalComputation fractions);
 		void output(FractionalComputation fractions);
-		fraction user_input[2],
-				 answer;
+		fraction frac[2], answer;
 	private:
 		// All of the formulas for this assignment are given in the book
 		fraction fractional_additon(FractionalComputation fractions);
 		fraction fractional_subtraction(FractionalComputation fractions);
 		fraction fractional_multiplication(FractionalComputation fractions);
 		fraction fractional_division(FractionalComputation fractions);
+		bool sanatize_input(FractionalComputation fractions);
 		bool fractional_comparison(FractionalComputation fractions);
 		int choice;
 };
 
 void FractionalComputation::input(FractionalComputation fractions)
 {
-	// Get the user's fractions
-	cout << "Numerator of fraction 1: ";
-	cin >> user_input[0].numerator;
-	cout << "Denominator of fraction 1: ";
-	cin >> user_input[0].denominator;
-	cout << "Numerator of fraction 2: ";
-	cin >> user_input[1].numerator;
-	cout << "Denominator of fraction 2: ";
-	cin >> user_input[1].denominator;
+	char input_char[21];
 
-	// Find out what they want to do with those fractions
-	cout << "What would you like to do with these fractions?" << endl
-		 << "1. Add" << endl
-		 << "2. Subtract" << endl
-		 << "3. Multiply" << endl
-		 << "4. Division" << endl
-		 << "5. Compare" << endl;
-	cin >> choice;
+	// Get the user's fractions
+	// Make them do it over if there is an error
+	do {
+		for (int i = 0; i < 2; i++) {
+			cout << "Fraction " << (i + 1) << ": ";
+			cin.getline(input_char, 21, '/');
+			frac[i].numerator = atoi(input_char);
+			cin.getline(input_char, 21, '\n');
+			frac[i].denominator = atoi(input_char);
+		}
+
+		// Find out what they want to do with those fractions
+		cout << "What would you like to do with these fractions?" << endl
+			 << "1. Add" << endl
+			 << "2. Subtract" << endl
+			 << "3. Multiply" << endl
+			 << "4. Division" << endl
+			 << "5. Compare" << endl;
+		cin >> choice;
+	} while (!sanatize_input(fractions));
 }
 
 void FractionalComputation::output(FractionalComputation fractions)
 {
 	cout << endl
-		 << "Fraction 1: " << user_input[0].numerator << "/" << user_input[0].denominator << endl
-		 << "Fraction 2: " << user_input[1].numerator << "/" << user_input[1].denominator << endl;
+		 << "Fraction 1: " << frac[0].numerator << "/" << frac[0].denominator << endl
+		 << "Fraction 2: " << frac[1].numerator << "/" << frac[1].denominator << endl;
 
 	// Switch on the user's choice to do mathematical operation
 	switch (choice) {
@@ -67,7 +76,7 @@ void FractionalComputation::output(FractionalComputation fractions)
 			fractional_multiplication(fractions);
 			break;
 		case 4:
-			 fractional_division(fractions);
+			fractional_division(fractions);
 			break;
 		case 5:
 			if (fractional_comparison(fractions))
@@ -85,36 +94,49 @@ void FractionalComputation::output(FractionalComputation fractions)
 
 fraction FractionalComputation::fractional_additon(FractionalComputation fractions)
 {
-	answer.numerator = (user_input[0].numerator * user_input[1].denominator) + (user_input[1].numerator * user_input[0].denominator);
-	answer.denominator = user_input[0].denominator * user_input[1].denominator;
+	answer.numerator = (frac[0].numerator * frac[1].denominator) + (frac[1].numerator * frac[0].denominator);
+	answer.denominator = frac[0].denominator * frac[1].denominator;
 	return answer;
 }
 
 fraction FractionalComputation::fractional_subtraction(FractionalComputation fractions)
 {
-	answer.numerator = (user_input[0].numerator * user_input[1].denominator) - (user_input[1].numerator * user_input[0].denominator);
-	answer.denominator = user_input[0].denominator * user_input[1].denominator;
+	answer.numerator = (frac[0].numerator * frac[1].denominator) - (frac[1].numerator * frac[0].denominator);
+	answer.denominator = frac[0].denominator * frac[1].denominator;
 	return answer;
 }
 
 fraction FractionalComputation::fractional_multiplication(FractionalComputation fractions)
 {
-	answer.numerator = user_input[0].numerator * user_input[1].numerator;
-	answer.denominator = user_input[0].denominator * user_input[1].denominator;
+	answer.numerator = frac[0].numerator * frac[1].numerator;
+	answer.denominator = frac[0].denominator * frac[1].denominator;
 	return answer;
 }
 
 fraction FractionalComputation::fractional_division(FractionalComputation fractions)
 {
-	answer.numerator = user_input[0].numerator * user_input[1].denominator;  
-	answer.denominator = user_input[0].denominator * user_input[1].numerator;
+	answer.numerator = frac[0].numerator * frac[1].denominator;  
+	answer.denominator = frac[0].denominator * frac[1].numerator;
 	return answer;
 }
 
 bool FractionalComputation::fractional_comparison(FractionalComputation fractions)
 {
-	if ((user_input[0].numerator / user_input[0].denominator) < (user_input[1].numerator / user_input[1].denominator))
+	FractionalComputation::fractional_multiplication(fractions);
+
+	if (answer.numerator < answer.denominator)
 		return false;
 	else
 		return true;
+}
+
+bool FractionalComputation::sanatize_input(FractionalComputation fractions)
+{
+	// Disallow division by zero
+	if (frac[0].denominator == 0 || frac[1].denominator == 0) {
+		cout << "Division by 0 is not allowed. Aborting." << endl;
+		return false;
+	}
+
+	return true;
 }
